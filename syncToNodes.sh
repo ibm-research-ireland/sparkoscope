e66ea8356a031ed0b5d5198973485c7bd2c46938
Yiannis Gkoufas <yiannisg@ie.ibm.com>
2015-11-26 00:50:26 +0000

    removed deploy scripts

--- syncToNodes.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-rsync -azP /disk/spark-viz/ shamrock066:/disk/spark-viz
-rsync -azP /disk/spark-viz/ shamrock067:/disk/spark-viz
-rsync -azP /disk/spark-viz/ shamrock068:/disk/spark-viz
-/disk/spark-viz/sbin/start-all.sh
