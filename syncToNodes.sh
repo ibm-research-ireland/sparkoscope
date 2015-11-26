bb47caf776ee5637059726154d9dcf2c36f0991e
Yiannis Gkoufas <yiannisg@ie.ibm.com>
2015-11-26 00:45:55 +0000

    deploy scripts again

--- syncToNodes.sh
+++ syncToNodes.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+
+rsync -azP /disk/spark-viz/ shamrock066:/disk/spark-viz
+rsync -azP /disk/spark-viz/ shamrock067:/disk/spark-viz
+rsync -azP /disk/spark-viz/ shamrock068:/disk/spark-viz
+/disk/spark-viz/sbin/start-all.sh
